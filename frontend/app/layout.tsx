10f3c9e6641fc38d8b3a4a3857efca26519cf3d5
Pallabi kar <kpallabi918@gmail.com>
2026-09-16 11:12:59 +0530

    feat: add Meta Pixel tracking code to root layout

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -164,6 +164,33 @@ export default function RootLayout({
           strategy="afterInteractive"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(schema) }}
         />
+        <Script
+          id="meta-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '2380781365697806');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=2380781365697806&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
       </head>
       <body className="bg-black flex justify-center">
         <main className="w-full ">
